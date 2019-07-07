af65d5dee9f9cde8024b0c2d50b1f6be878d228c
mahajan-apoorv <52637083+mahajan-apoorv@users.noreply.github.com>
2019-07-08 02:34:05 +0530

    Add files via upload

---
+++ sketch_new1.ino
@@ -0,0 +1,115 @@
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+SoftwareSerial mySerial2(3,2);
+/************************* Adafruit.io Setup *********************************/
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "iec2016057"               // Adafruit IO Username 
+#define AIO_KEY         "89e9fded9a6449a5a82acaaa791790c5" // Adafruit IO Key
+
+int sensor[5]; 
+int i=0;
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish vib = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/vibration");
+Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+Adafruit_MQTT_Publish current = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/current");
+Adafruit_MQTT_Publish vol = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/voltage");
+Adafruit_MQTT_Publish rpm= Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/speed");
+
+// Setup a feed called 'Sensor_lock' for subscribing to changes.
+//Adafruit_MQTT_Subscribe Sensor_lock = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/Sensor_lock");
+
+/*************************** Sketch Code ************************************/
+
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(9600);
+  mySerial2.begin(9600);
+  pinMode(3,INPUT);
+  pinMode(2,OUTPUT);
+  //delay(100);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+
+
+  WiFi.begin("Test","aaaaaaaa");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+uint32_t x=0;
+
+ 
+void loop() {
+
+if (mySerial2.available())
+{
+  MQTT_connect();
+  
+  for (i=0;i<5;i++)
+  {
+    sensor[i] = mySerial2.parseInt();
+  }   
+  delay(2000);
+  vib.publish(sensor[0]);
+  delay(2000);
+  temp.publish(sensor[1]);
+  delay(2000);
+  current.publish(sensor[2]);
+  delay(2000);
+  vol.publish(sensor[3]);
+  delay(2000);
+  rpm.publish(sensor[4]);
+  
+  }
+}
+void MQTT_connect() {
+  int8_t ret;
+
+  if (mqtt.connected()) return;
+
+  //Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) 
+  { 
+    // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(3000);  // wait 3 seconds
+       retries--;
+       if (retries == 0) {
+         
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+
---
+++ sketch_new2a.ino
@@ -0,0 +1,178 @@
+#include "TimerOne.h"
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial1(9,8);
+int temp_raw_value;
+long int sum = 0;
+long int sum1 = 0;
+int EP=3;
+ int i;
+int currentPin = A1;
+int sensitivity = 65;
+int adcValue= 0;
+int offsetVoltage = 2500;
+float adcVoltage = 0;
+float currentValue;
+int vibr_Pin =12;
+int temp;
+float vol_raw_value=0;
+long long int avg=0;
+
+unsigned int counter=0;
+int a[5]={0,0,0,0,0};
+
+void docount()
+{
+  counter++;  
+} 
+
+void timerIsr()
+{
+  Timer1.detachInterrupt();  //stop the timer
+  int rotation = (counter / 2)*60;  // divide by number of holes in Disc
+  a[4] = rotation;
+  counter=0;  //  reset counter to zero
+  Timer1.attachInterrupt( timerIsr ); // enable the timer
+}
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  mySerial1.begin(9600);
+  delay(7000);
+  pinMode(vibr_Pin, INPUT);
+  pinMode(A1, INPUT);
+  Timer1.initialize(1000000); // set timer for 1sec
+  attachInterrupt(1, docount, RISING);  // increase counter when speed sensor pin goes High
+  Timer1.attachInterrupt( timerIsr ); // enable the timer
+   
+  
+}
+
+void loop() {
+  
+  long measurement = TP_init();
+
+  a[0] = measurement;                         //vibration
+
+                                                                                          //temp sensor
+  for(int i=1;i<=50;i++)
+  {
+  temp_raw_value = analogRead(A0);
+  temp_raw_value = map(temp_raw_value,0,1023,0,5050)+20;
+  sum = sum + temp_raw_value;
+  }
+
+  temp_raw_value = sum/50;
+
+  a[1] = temp_raw_value/10;
+  sum = 0;
+ 
+   for (i =0;i<50;i++) {                                                                                   //current sensor
+  adcValue = analogRead(currentPin);
+  //Serial.println(adcValue);
+  adcVoltage = (adcValue / 1024.0) * 5000;
+  //Serial.println(adcVoltage);
+  currentValue = ((( adcVoltage - offsetVoltage) / sensitivity)*1000)+150;
+  sum1 = sum1 + currentValue;
+  }
+  
+  currentValue = (sum1/50);
+  a[2] = currentValue;
+  sum1= 0;
+  
+  for(i=1;i<=50;i++)                                                                   //voltage 
+  {
+  vol_raw_value = analogRead(A2);
+  vol_raw_value = map(vol_raw_value,0,1023,0,5200);
+  sum = sum + vol_raw_value;
+  }
+  a[3] = (sum/50)+100;
+  sum = 0;
+    
+  
+ Serial.print("vib "); 
+ Serial.println(a[0]);
+ 
+ Serial.print("temp "); 
+ Serial.println(a[1]);
+ 
+ Serial.print("current "); 
+ Serial.println(a[2]);
+ 
+ Serial.print("voltage "); 
+ Serial.println(a[3]);
+ 
+ Serial.print("rpm "); 
+ Serial.println(a[4]);
+
+ Serial.println("\n");
+ 
+ for (i=0;i<5;i++)
+ { 
+  mySerial1.println(a[i]);
+  //mySerial1.println('\n');
+  }
+  
+  if (a[0]> 1000)
+  {
+    digitalWrite(4,HIGH);
+    digitalWrite(5,HIGH);
+  }
+  else
+  {
+   // digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+  }
+  if (a[1]>=32)
+  {
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+  }
+   else
+  {
+//    digitalWrite(4,LOW);
+    digitalWrite(6,LOW);
+  }
+  if (a[2]> 1000)
+  {
+    digitalWrite(4,HIGH);
+    digitalWrite(7,HIGH);
+  }
+   else
+  {
+  //  digitalWrite(4,LOW);
+    digitalWrite(7,LOW);
+  }
+  if (a[3]> 1400)
+  {
+    digitalWrite(4,HIGH);
+    digitalWrite(10,HIGH);
+  }
+   else
+  {
+    //digitalWrite(4,LOW);
+    digitalWrite(10,LOW);
+  }
+  if (a[4]> 7000 )
+  {
+    digitalWrite(4,HIGH);
+    digitalWrite(11,HIGH);
+  }
+   else
+  {
+    //digitalWrite(4,LOW);
+    digitalWrite(11,LOW);
+  }
+  if(a[0]<1000&&a[1]<32 && a[2]<1000 &&a[3]<1400 && a[4]<7000)
+  {
+    digitalWrite(4,LOW);
+  }
+
+   delay(13000);
+}
+long TP_init(){
+  long measurement=pulseIn (EP, HIGH);  //wait for the pin to get HIGH and returns measurement
+ // measurement=measurement/1000;
+  return measurement;
+}
